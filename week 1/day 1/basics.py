3b110e06e38009bdaa636624c86ff062ddc29274
Aryan Singh <aryanbaghi076@gmail.com>
2026-01-19 18:16:53 +0530

    starting

---
+++ week 1/day 1/basics.py
@@ -0,0 +1,23 @@
+print("Day 1: Python for AI/ML");
+
+# Arithmetic Operations (except complex where noted)
+
+# + , - , * , / → normal arithmetic
+
+# // → floor division (rounds down)
+
+# % → remainder
+
+# ** or pow() → power
+
+# abs() → absolute value (magnitude for complex)
+
+# divmod(x, y) → (x//y, x%y)
+
+# Special points:
+
+# // always rounds towards −∞
+
+# int(float) truncates decimal part
+
+# 0 ** 0 = 1 in Python
