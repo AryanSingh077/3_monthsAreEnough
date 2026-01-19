a0c01131d73e8440260ff09a6d4394a646350317
Aryan Singh <aryanbaghi076@gmail.com>
2026-01-19 18:28:31 +0530

    basic datatypes, typesa and conversions

--- week 1/day 1/basics.py
+++ week 1/day 1/basics.py
@@ -21,3 +21,27 @@ print("Day 1: Python for AI/ML");
 # int(float) truncates decimal part
 
 # 0 ** 0 = 1 in Python
+
+# int. float, str, bool, type() and type conversion
+
+age = 19;
+height = 5.9
+name = "Aryan";
+isStudent = True;
+
+print(type(age));
+print(type(height));
+print(type(name));
+print(type(isStudent));
+
+input1 = int(input("Enter your age. : "))
+input2 = float(input("Enter your height. : "))
+input3 = str(input("Enter your name. : "))
+input4 = bool(input("Enter your isStudent. : "))
+
+print(f"My name is{name}, im {age} yrs old, my height is {height}, and im a {isStudent}");
+print(type(input1));
+print(type(input2));
+print(type(input3));
+print(type(input4));
+
