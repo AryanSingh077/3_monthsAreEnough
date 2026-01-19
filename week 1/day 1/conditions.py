53460665da2cc18b167e6e9c4c44507c5541f2dc
Aryan Singh <aryanbaghi076@gmail.com>
2026-01-19 22:55:55 +0530

    ongoin conditions

---
+++ week 1/day 1/conditions.py
@@ -0,0 +1,23 @@
+# # 1: grade calculator:
+
+# a = float(input("Enter your marks in maths: "))
+# b = float(input("Enter your marks in chemistry: "))
+# c = float(input("Enter your marks in english: "))
+# d = float(input("Enter your marks in hindi: "))
+# e = float(input("Enter your marks in physics: "))
+
+# avgMarks = float((a+b+c+d+e)/5);
+# print(f"your average marks is {avgMarks}.");
+
+# if avgMarks >= 85:
+#     print("your grade is A");
+# elif avgMarks >= 70:
+#     print("your grade is B");
+# elif avgMarks >= 55:
+#     print("your grade is C");
+# elif avgMarks >= 40:
+#     print("Your grade is D");
+# else:
+#     print("Failed in examination!!");
+
+# 
