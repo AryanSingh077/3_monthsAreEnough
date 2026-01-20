e9676c1428fa33a09c142273d48ba2f93f06b813
Aryan Singh <aryanbaghi076@gmail.com>
2026-01-20 17:25:46 +0530

    for loop done

---
+++ week 1/day 1/forLoop.py
@@ -0,0 +1,25 @@
+# #prob1:
+
+# for i in range(1,101):
+#     print(i)
+
+# #prob2:
+
+# sum = 0
+# i = 1
+# for i in range(1,101):
+#     sum = sum + i
+#     print(sum)
+
+# #prob3:
+
+# n = int(input("Enter the no.: "))
+
+# for i in range(1,11):
+#     print(f"{n}x{i}= {n*i}")
+
+#prob4:
+
+lst = ["aryan", 45, "tell me", "hey"]
+for i in lst:
+    print(i)
